40ccc4f382faf5d5cb53882ffa56374d7121992e
Michael Carmelo <michaelcarmelo22@gmail.com>
2023-11-20 19:38:01 -0700

    Got rid of example blocks, added AM/PM

--- Develop/script.js
+++ Develop/script.js
@@ -9,17 +9,17 @@ $(function () {
   }
 
   function colorCodeTimeBlocks() {
-    var currentHour = dayjs().format('H');
+    var currentHour = dayjs().hour();
 
     $(".time-block").each(function () {
       var blockHour = parseInt($(this).attr("id").split("-")[1]);
 
       if (blockHour < currentHour) {
-        $this.removeClass("present future").addClass("past");
+        $(this).removeClass("present future").addClass("past");
       } else if (blockHour === currentHour) {
-        $this.removeClass("past future").addClass("present");
+        $(this).removeClass("past future").addClass("present");
       } else {
-        $this.removeClass("past present").addClass("future");
+        $(this).removeClass("past present").addClass("future");
       }
     });
   }
@@ -35,8 +35,8 @@ $(function () {
       var blockId = $(this).attr("id");
       var savedEventText = localStorage.getItem(blockId);
 
-      if (savedEvent) {
-        $(this.find(".description")).val(savedEvent);
+      if (savedEventText) {
+        $(this).find(".description").val(savedEventText);
       }
     });
   }
@@ -47,12 +47,19 @@ $(function () {
     for (var i = 9; i <= 17; i++) {
       var timeBlock = $("<div>").attr("id", "hour-" + i).addClass("row time-block");
       var hourDiv = $("<div>").addClass("col-2 col-md-1 hour text-center py-3").text(i + "AM");
+      var period = i < 12 ? "AM" : "PM";
+      var displayHour = i <= 12 ? i : i - 12;
+      hourDiv.text(displayHour + " " + period);
+
+      var saveButton = $("<button>").addClass("btn saveBtn col-2 col-md-1").attr("aria-label", "save")
+        .append($("<i>").addClass("fas fa-save").attr("aria-hidden", "true"));
       var textArea = $("<textarea>").addClass("col-8 col-md-10 description").attr("rows", "3")
         .append($("<i>").addClass("fas fa-save").attr("aria-hidden", "true"));
 
-        timeBlock.append(hourDiv, textArea, saveBtn);
+        timeBlock.append(hourDiv, textArea, saveButton);
         container.append(timeBlock);
     }
+    $(".row.time-block.example").remove();
   }
 
   updateCurrentDate();
